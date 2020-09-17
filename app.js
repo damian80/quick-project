90bc7cd6a7e145b9b50d76d254edc00b67f11475
Damian Ciasnocha <daneek80@gmail.com>
2020-09-17 23:02:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // pobieramy przycisk tworzac zmienna const
-const btn = document.querySelector('button');
+const btn = document.querySelector("button");
 // podpinamy do niego nasluchiwanie na click i tworzymy funkcje
-btn.addEventListener('click', function () {
-
-})
+const addElement = function() {
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+};
+btn.addEventListener("click", addElement);
+// zadeklarujemy funkcje
