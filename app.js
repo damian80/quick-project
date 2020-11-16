e6964d754078c6f53fafbf5e18117c49d4e1457e
Damian Ciasnocha <daneek80@gmail.com>
2020-11-16 22:24:23 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let number = 1;
 const addElement = function() {
   const div = document.createElement("div");
   div.textContent = number;
+  if (number % 5 == 0) {
+    div.classList.add("circle");
+  }
   document.body.appendChild(div);
   number++;
 };
